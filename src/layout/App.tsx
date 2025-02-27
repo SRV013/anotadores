089f9bff42d37c165f6a25605105e2c6b0eab19d
SRV013 <srv013@gmail.com>
2025-02-27 01:14:27 -0300

    style: update primary color and add blurred background image styling

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -4,9 +4,11 @@ import './App.css'
 interface LayOutProps {
   children: ReactNode;
 }
+// import img  from "@/asset/backgroud.png";
 function LayOut({ children }: LayOutProps) {
   return (
     <div id="app">
+      {/* <img src={@/asset/backgroud.png} alt="asa" /> */}
       <div id="background" />
       <section id="container">
         {children}
